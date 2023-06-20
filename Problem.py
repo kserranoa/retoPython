b65d1feb93b2436849df249c7123dcae37ada567
Karina Serrano <52381907+kserranoa@users.noreply.github.com>
2023-06-20 16:10:25 -0600

    Create Problem.py

---
+++ Problem.py
@@ -0,0 +1,26 @@
+File Navigation and Stream Processing
+The following exercise will prepare you for the on-site challenge, so roll up your sleeves, get your coding hat on and let's dive in!
+
+
+
+Scenario: You are the Scranton Regional Manager and Christmas is fast approaching, which means it's time to give out bonuses. However, the task of analyzing the sales reports for the year has fallen on your shoulder, but don't worry, we'll be using Python to make this task a breeze!
+
+To complete this task, you will need to:
+
+Identify the sales reports made by the Scranton sales team.
+
+These files will all end with the suffix "_salesman."
+Collect all of the identified sales reports and place them in a single folder.
+
+Look for specific details within each file to determine if it belongs to the Scranton branch.
+Calculate the total sales from all of the Scranton sales reports and create a new file to store this information.
+
+Give it a try first!
+You will find the solution with a guided recording in the resource center, but I highly encourage you to attempt to solve the problem on your own first.
+
+Remember, the most crucial aspect of programming is the thought process, so it's not crucial whether your code works or not, as long as you're learning and enjoying the process!
+
+
+
+Reminder
+Don't forget to create the folder in the left panel and add the input files provided before executing your code.
